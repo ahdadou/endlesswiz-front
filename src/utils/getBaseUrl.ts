3c9e60518459e86b82c0764e1c24b27074e446e8
ahdadou <ibra.ahdadou@gmail.com>
2025-03-22 14:46:49 +0000

    fix url

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,11 +1,11 @@
 export const getBaseUrl = (): string => {
   let baseUrl: string;
   if (typeof window === "undefined") {
-    baseUrl = "http://localhost:8099/api/v1";
+    baseUrl = "http://localhost:80/api/v1";
   } else {
     const isLocalhost = window.location.origin.includes("localhost");
     baseUrl = isLocalhost
-      ? "http://localhost:8099/api/v1"
+      ? "http://localhost:80/api/v1"
       : window.location.origin;
   }
   return baseUrl;
