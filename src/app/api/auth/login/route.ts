5cc54c1c7c2b2ee3e1aed8b8ebd8428b0333246c
ahdadou <ibra.ahdadou@gmail.com>
2025-03-24 05:02:18 +0000

    temporary

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -7,10 +7,13 @@ export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
 
+    console.log('### LOGIN url ------>  ',`${getBaseUrl()}/auth/authenticate` )
     const response = await axios.post(
       `${getBaseUrl()}/auth/authenticate`,
       { email, password }, // Send as a JavaScript object (no need to stringify)
     );
+
+    console.log('### LOGIN res   ---> ', response)
 
     if (response.status !== 200) {
       return NextResponse.json(
