4e9445787b2c585c2b6ccf62f18a7c47c60bdfe5
ahdadou <ibra.ahdadou@gmail.com>
2025-03-24 01:17:05 +0000

    configure https

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,13 +1,13 @@
 export const getBaseUrl = (): string => {
   let baseUrl: string;
   if (typeof window === "undefined") {
-    baseUrl = "http://www.api.endlesswiz.com/api/v1";
+    baseUrl = "https://www.api.endlesswiz.com/api/v1";
   } else {
     // const isLocalhost = window.location.origin.includes("localhost");
     // baseUrl = isLocalhost
     //   ? "http://www.api.endlesswiz.com/api/v1"
     //   : window.location.origin;
-    baseUrl = "http://www.api.endlesswiz.com/api/v1";
+    baseUrl = "https://www.api.endlesswiz.com/api/v1";
   }
   return baseUrl;
 };
